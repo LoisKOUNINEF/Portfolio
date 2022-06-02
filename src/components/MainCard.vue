79eb0c653aa921b4cc9bad46e8b6452ea6e8393c
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-02 14:13:37 +0200

    main card structure (no values/colors)

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,6 +1,58 @@
 <template>
   <div class="main-container">
-    <h1>Mon Portfolio</h1>
+    <div class="first-container">
+      <div class="profile">
+        <img src="" alt="profile picture">
+        <h1>Loïs Kouninef</h1>
+        <h4>Web Developper</h4>
+      </div>
+      <div class="social-links">
+        <div class="link">
+          <a href="">
+            <img src="" alt="">
+            <p>GitHub</p>
+          </a>
+        </div>
+      </div>
+      <div class="social-links">
+        <div class="link">
+          <a href="">
+            <img src="" alt="">
+            <p>LinkedIn</p>
+          </a>
+        </div>
+      </div>
+      <div class="social-links">
+        <div class="link">
+          <a href="">
+            <img src="" alt="">
+            <p>Medium? Email?</p>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="second-container">
+      <div class="about-me">
+        <h2>A Propos</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, eum? Ipsam iure similique esse dignissimos rerum quia sed, debitis sint!</p>
+      </div>
+      <div class="technologies">
+        <h2>Technologies</h2>
+        <div class="logos">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+        </div>
+      </div>
+      <div class="button-container">
+        <div class="project-button">
+          <a href="">Mes projets</a>
+        </div>
+        <div class="project-button">
+          <a href="">Mon CV</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +63,55 @@
 </script>
 
 <style scoped>
+
+  a{
+    text-decoration: none;
+    color: ;
+  }
+
+  .main-container {
+    display: flex;
+  }
+
+  .first-container {
+    background-color: ;
+    width: ;
+    padding: ;
+    border-radius: 25px;
+  }
+
+  .second-container {
+    background-color: ;
+    width: ;
+    padding: ;
+    border-radius: 25px;
+  }
+
+  .profile img {
+    height: ;
+    border-radius: 50%;
+  }
+
+  .link img {
+    height: ;
+  }
+
+  .logos img {
+    height: ;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .project-button {
+    background-color: ;
+    padding: ;
+    margin: ;
+    border-radius: ;
+  }
+
 
 </style>
