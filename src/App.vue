1152832bbfeafc5480bee287a2d04ce857068a0e
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-02 23:12:06 +0200

    add Roboto font

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <MainCard />
   <ProjectsCard />
 </template>
 
 <script>
-import MainCard from './components/MainCard.vue'
-import ProjectsCard from './components/ProjectsCard.vue'
+  import MainCard from './components/MainCard.vue'
+  import ProjectsCard from './components/ProjectsCard.vue'
 
-export default {
-  name: 'App',
-  components: {
-    MainCard,
-    ProjectsCard
+  export default {
+    name: 'App',
+    components: {
+      MainCard,
+      ProjectsCard
+    }
   }
-}
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
   body {
     margin: 0;
     background-color: #ccc;
   }
-#app {
-  font-family: Helvetica, Avenir, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #ccc;
-}
+  #app {
+    font-family: "Roboto", Helvetica, Avenir, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: #ccc;
+  }
 </style>
