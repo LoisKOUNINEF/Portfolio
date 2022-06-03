500347f144b7bb14a828b570b3f4dd0f8b7ab2ae
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-03 18:08:46 +0200

    displaying provisionary pdf in resume page

--- src/components/ResumeCard.vue
+++ src/components/ResumeCard.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <h2>Le CV est en cours de réalisation !</h2>
-    <p>Coming soon...</p>
+  <div class="container">
+    <div class="button-container">
+            <div class="project-button">
+        <router-link to ="/">Retourner à l'accueil</router-link>
+      </div>
+      <div class="project-button">
+        <router-link to ="/projects">Voir les projets</router-link>
+      </div>
+    </div>
+    <div class="pdf-container">
+      <iframe class="pdf" src="Resume.pdf#toolbar=0&navpanes=0&scrollbar=0" type="application/pdf" frameborder="0" width="100%" height="100%" scrolling="auto"></iframe>
+    </div>
   </div>
 </template>
 
@@ -11,6 +20,40 @@
   }
 </script>
 
-<style>
+<style scoped>
 
+  .container {
+    background: linear-gradient( #849FDE, #5B82DE 15%);
+  }
+
+  .pdf-container {
+    height: 40em;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding-top: 4em;
+    margin-bottom: 3em;
+  }
+
+  .project-button {
+    background: linear-gradient(30deg, #00319C, #0050FF);
+    padding: 1em;
+    font-weight: 600;
+    margin-top: 1em;
+    border-radius: 100px;
+  }
+
+  .project-button a {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  @media only screen and (max-width: 900px) {
+    .pdf {
+
+    }
+  }
 </style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <MainCard />
+
   <ResumeCard />
 </template>
 
 <script>
-import MainCard from '@/components/MainCard.vue'
+
 import ResumeCard from '@/components/ResumeCard.vue'
 
 export default {
   name: 'ResumeView',
   components: {
-    MainCard,
+
     ResumeCard
   }
 }
