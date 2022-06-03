6a2a2dd0a28b3f99ca4c0aeeaa31ce23ad9136c0
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-03 18:36:24 +0200

    lorem pdf resume

--- src/components/ResumeCard.vue
+++ src/components/ResumeCard.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="pdf-container">
-      <iframe class="pdf" src="Resume.pdf#navpanes=0&scrollbar=0&view=fitH" type="application/pdf" frameborder="0" scrolling="auto"></iframe>
+      <iframe class="pdf" src="resume.pdf#navpanes=0&scrollbar=0&view=fitH" type="application/pdf" frameborder="0" scrolling="auto"></iframe>
     </div>
   </div>
 </template>
