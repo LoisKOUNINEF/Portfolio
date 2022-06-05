d7d48ccabb179cd615f2b62f879ebd548a2f3e0f
LoisKOUNINEF <lkouninef@gmail.com>
2022-06-05 19:31:58 +0200

    border-box

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
 
 <style>
 
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
   }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #111213;
-  background-color: #BBCDF5
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #111213;
+    background-color: #BBCDF5
+  }
 
 </style>
